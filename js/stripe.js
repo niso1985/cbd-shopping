bde93dada1d5a1f36c31504fce06bd9841848b09
niso1985 <niso1985@gmail.com>
2019-11-06 00:15:44 +0900

    update: fist beta release

--- js/stripe.js
+++ js/stripe.js
@@ -1,68 +1,43 @@
-(function () {
-    var stripe = Stripe('pk_test_6dgYUoFefhkUiXQos4EmPEaL');
-    var elements = stripe.elements();
+var createOrder = function() {
+    var button = document.getElementById("order");
+    button.classList.add("is-loading");
+    var cancelBtn = document.getElementById("cancel")
+    cancelBtn.disabled = true;
+    var shipping = document.getElementById("shipping")
+    var success = document.getElementById("success")
+    var failed = document.getElementById("failed")
 
-    // Element作成時に options から スタイルを調整できます.
-    var style = {
-        base: {
-          color: '#32325d',
-          fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
-          fontSmoothing: 'antialiased',
-          fontSize: '16px',
-          '::placeholder': {
-            color: '#aab7c4'
-          }
-        },
-        invalid: {
-          color: '#fa755a',
-          iconColor: '#fa755a'
-        }
-      };
-
-    // card Element のインスタンスを作成
-    var card = elements.create('card', { style: style });
-
-    // マウント
-    card.mount('#card-element');
-
-    card.addEventListener('change', function (event) {
-        var displayError = document.getElementById('card-errors');
-        if (event.error) {
-            displayError.textContent = event.error.message;
+    var url = "https://script.google.com/macros/s/AKfycbwH_Nb-mvmOJ_kLympVuHVFzgAafLOI69G-Vy6UwGlOf5JZqSE4/exec"
+    var data = {
+        name: "aaa",
+        email: "bbb",
+        tell: "ccc",
+        postalcode: "ddd",
+        address: "eee",
+        oil: 1,
+        cream: 2,
+        rollon: 0,
+        pack: 0
+    }
+    var request = new XMLHttpRequest();
+    request.open('POST', url);
+    request.setRequestHeader("Content-Type", "application/json");
+    request.onreadystatechange = function () {
+        if (request.readyState != 4) {
+            // リクエスト中
+        } else if (request.status != 200) {
+            button.classList.remove("is-loading");
+            button.disabled = true;
+            shipping.classList.add("is-hidden")
+            failed.classList.remove("is-hidden")
+            cancelBtn.disabled = false;
         } else {
-            displayError.textContent = '';
+            button.classList.remove("is-loading");
+            button.disabled = true;
+            shipping.classList.add("is-hidden")
+            success.classList.remove("is-hidden")
+            cancelBtn.disabled = false;
         }
-    });
-
-    //トークン作成もしくはエラー表示
-    var form = document.getElementById('payment-form');
-    form.addEventListener('submit', function (event) {
-        event.preventDefault();
-
-        stripe.createToken(card).then(function (result) {
-            if (result.error) {
-                // エラー表示.
-                var errorElement = document.getElementById('card-errors');
-                errorElement.textContent = result.error.message;
-            } else {
-                // トークンをサーバに送信
-                stripeTokenHandler(result.token);
-            }
-        });
-    });
-
-    function stripeTokenHandler(token) {
-        // tokenをフォームへ包含し送信
-        var form = document.getElementById('payment-form');
-        var hiddenInput = document.createElement('input');
-        hiddenInput.setAttribute('type', 'hidden');
-        hiddenInput.setAttribute('name', 'stripeToken');
-        hiddenInput.setAttribute('value', token.id);
-        form.appendChild(hiddenInput);
-
-        // Submit します
-        form.submit();
-    }    
-})();
-
-
+    };
+    request.send(JSON.stringify(data));
+}
